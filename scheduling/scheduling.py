afd249b7d21ad09fa92a05ad5bb59a87990c5384
lowie1803 <dung.totuan01@gmail.com>
2024-07-05 19:10:19 +0700

    Add configurations

---
+++ scheduling/scheduling.py
@@ -0,0 +1,41 @@
+import os
+from datetime import datetime
+
+OFFSET_ALLOWED = 60
+
+class Scheduler():
+  scheduler = {}
+
+  def __init__(self):
+    self.scheduler = {
+      "morning": {
+        "minute": os.getenv('MORNING_MINUTE') or 31,
+        "first_hour": os.getenv("MORNING_FIRST_HR") or 6,
+        "last_hour": os.getenv("MORNING_LAST_HR") or 16,
+      },
+      "evening": {
+        "minute": os.getenv('EVENING_MINUTE') or 1,
+        "first_hour": os.getenv("EVENING_FIRST_HR") or 17,
+        "last_hour": os.getenv("EVENING_LAST_HR") or 23,
+      }
+    }
+
+  def is_morning_pump_schedule(self):
+    tstamp = datetime.now()
+    s_morning = self.scheduler["morning"]
+    if tstamp.hour < s_morning["first_hour"] or tstamp.hour > s_morning["last_hour"]:
+      return False
+
+    seconds_passed = tstamp.minute * 60 + tstamp.second
+    target = s_morning["minute"] * 60
+    return target - OFFSET_ALLOWED <= seconds_passed <= target + OFFSET_ALLOWED
+
+  def is_evening_pump_schedule(self):
+    tstamp = datetime.now()
+    s_evening = self.scheduler["evening"]
+    if tstamp.hour < s_evening["first_hour"] or tstamp.hour > s_evening["last_hour"]:
+      return False
+
+    seconds_passed = tstamp.minute * 60 + tstamp.second
+    target = s_evening["minute"] * 60
+    return target - OFFSET_ALLOWED <= seconds_passed <= target + OFFSET_ALLOWED
